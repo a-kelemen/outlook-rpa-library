5b4523f6e1ebb08809699dc07694fd6f6412b3be
Andras Kelemen <kelemenandras11@gmail.com>
2019-05-11 10:03:40 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 import setuptools
 
 
-setuptools.setup(name='emailprocesslibrary',
-      version='0.1',
-      description='Library for outlook automation.',
-      url='',
-      author='Andras Kelemen',
-      author_email='kelemenandras11@gmail.com',
-      license='MIT',
-      packages=setuptools.find_packages(exclude=['EmailProcessLibrary.tests']),
+setuptools.setup(
+	name='emailprocesslibrary',
+	version='0.1',
+	description='Library for outlook automation.',
+	url='',
+	author='Andras Kelemen',
+	author_email='kelemenandras11@gmail.com',
+	license='MIT',
+	packages=setuptools.find_packages(exclude=['EmailProcessLibrary.tests']),
 
-      install_requires=[
-        'pywin32',
-        'robotframework',
-      ],
-      test_suite="tests",
-      tests_require=['nose'],
-      zip_safe=False
-      )
+	install_requires=[
+		'pywin32',
+		'robotframework',
+	],
+	test_suite="tests",
+	tests_require=['nose'],
+	zip_safe=False
+	)
